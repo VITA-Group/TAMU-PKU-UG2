f9fe7c2a42a975fa7216d5f09c614f1fc08f779b
Yueyu Hu <huyy@pku.edu.cn>
2018-06-14 15:48:08 +0800

    Add instructions to download pretrained models in hdrnet

---
+++ hdrnet/run_hdrnet.sh
@@ -0,0 +1,4 @@
+# python2 run.py ./pretrained_models/local_laplacian/strong_1024/  {input_dir} {output_dir}
+
+#example
+python2 run.py ./pretrained_models/local_laplacian/strong_1024/ ../example_src ../example_dst
