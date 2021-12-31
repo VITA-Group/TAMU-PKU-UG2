2c6ef76d029b76aa8542b8a78d0aab5d49fed216
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-31 17:54:19 +0800

    Delete hdrnet directory

--- hdrnet/bat_run.py
+++
@@ -1,6 +0,0 @@
-import os
-import sys
-
-
-if __name__ == "__main__":
-    os.system('python2 ./hdrnet/bin/run.py ./pretrained_models/local_laplacian/strong_1024/ '+sys.argv[0]+' '+ sys.argv[1])
--- hdrnet/run_hdrnet.sh
+++
@@ -1,4 +0,0 @@
-# python2 run.py ./pretrained_models/local_laplacian/strong_1024/  {input_dir} {output_dir}
-
-#example
-python2 run.py ./pretrained_models/local_laplacian/strong_1024/ ../example_src ../example_dst
